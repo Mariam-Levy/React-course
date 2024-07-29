684a9e8d503e3878de46680aae0a6c57314ee27a
Mariam-Levy <mariamlevycode@gmail.com>
2024-07-29 12:07:56 -0300

    complete final implementation of the pizza project

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -79,13 +79,25 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
-      ) : (<p>We're still working on our menu. Please come back later ☺</p>)}
+
+        <>
+          <p>Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.</p>
+
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+
+        </>
+      ) : (<p>We're still working on our menu. Please come back later ☺</p>
+
+
+      )}
 
 
       {/* <Pizza
@@ -107,19 +119,20 @@ function Menu() {
 }
 
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
 
-  if (props.pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
 
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -127,7 +140,7 @@ function Pizza(props) {
 
 
 function Footer() {
-  const hour = new Date().getHours()
+  const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
@@ -140,22 +153,28 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
-      ) : (<p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>)
-      }
-    </footer >
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
   );
 
   // return React.createElement('footer', null, "We're currently open!")
 }
 
-function Order(props) {
-  return <div className="order">
-    <p>
-      We're open until {props.closeHour}:00. Come visit us or order online.
-    </p>
-    <button className="btn">Order</button>
-  </div>
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 
@@ -166,20 +185,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-/* 
-en react hay dos reglas importantes cuando escribimos componentes como funciones:
-    1.- el nombre de la funcion tiene que empezar con una letra mayuscula.
-    2.- la funcion tiene que devolver alguna marca, por lo general en forma de JSX.
-NOTA: no se puede devolver null.
-    - un componente solo puede devolver exactamente un elemento y no dos elementos, esto puede arrojarnos un error. Al menos que encerremos dichos elementos dentro de un div.
-    
-    
-    - no podemos usar las clases porque es una palabra reservada en JS, por ello usaremos "className"
-*/
-
-/* 
-renderizar una lista (rendering lists): es cuando tenemos array y queremos crear un componente por cada elemento del array
-*/
 
