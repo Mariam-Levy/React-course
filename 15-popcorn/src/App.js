e9b5084eef3b40a30259496c51cfbdf8700688a1
Mariam-Levy <mariamlevycode@gmail.com>
2024-08-21 16:34:47 -0300

    fixing prop

--- 15-popcorn/src/App.js
+++ 15-popcorn/src/App.js
@@ -56,19 +56,26 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar movies={movies} >
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox >
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
 /* _________________________________________ */
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   )
 }
@@ -106,17 +113,16 @@ function NumResults({ movies }) {
 /* ____________________________________ */
 
 
-function Main({ movies }) {
+function Main({ children }) {
 
   return (
     <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
+      {children}
     </main>
   )
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -127,9 +133,8 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && (
-        <MovieList movies={movies} />
-      )}
+      {isOpen1 && children
+      }
     </div>
   )
 }
